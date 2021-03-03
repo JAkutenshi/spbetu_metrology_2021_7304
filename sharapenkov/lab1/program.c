e557bf0c1dd6414f654a9d1d8e9b3212cf4a44e4
smokfyz <ivansharapenkov@gmail.com>
2021-03-03 10:27:52 -0800

    Add C and ASM code

--- sharapenkov/lab1/program.c
+++ sharapenkov/lab1/program.c
@@ -1,1 +1,68 @@
-c
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>  
+
+double x, er, ec;
+bool done;
+
+double erf(double x) {
+    const double sqrtpi = 1.7724538;
+    const double tol = 1.0E-4;
+
+    double x2, sum, sum1, term;
+    int i;
+
+    x2 = x * x;
+    sum = x;
+    term = x;
+    i = 0;
+    do {
+        i = i + 1;
+        sum1 = sum;
+        term = 2.0 * term * x2 / (1.0 + 2.0 * i);
+        sum = term + sum1;
+    } while(term >= tol * sum);
+    return 2.0 * sum * exp(-x2) / sqrtpi;
+}
+
+double erfc(double x) {
+    const double sqrtpi = 1.7724538;
+    const int terms = 12;
+
+    double x2, u, v, sum;
+    int i;
+    x2 = x * x;
+    v = 1.0 / (2.0 * x2);
+    u = 1.0 + v * (terms + 1.0);
+    for(i = terms; i; --i) {
+        sum = 1.0 + i * v / u;
+        u = sum;
+    }
+    return exp(-x2) / (x * sum * sqrtpi);
+}
+
+int main() {
+    bool done = false;
+    double x;
+    do {
+        printf("Arg? ");
+        scanf("%lf", &x);
+        if(x < 0.0) {
+            done = true;
+        } else {
+            if(x == 0.0) {
+                er = 0.0;
+                ec = 1.0;
+            } else {
+                if(x < 1.5) {
+                    er = erf(x);
+                    ec = 1.0 - er;
+                } else {
+                    ec = erfc(x);
+                    er = 1.0 - ec;
+                }
+            }
+            printf("X= %8.4lf Erf= %12.8lf Erfc= %12lf\n", x, er, ec);
+        }
+    } while(!done);
+}
--- sharapenkov/lab1/program.pas
+++ sharapenkov/lab1/program.pas
@@ -1,7 +1,8 @@
 program erfd3;
 { evaluation of the gaussian error function }
+uses Crt;
 
-var	x,er,ec	: real;
+var	x, er, ec	: real;
     done : boolean;
 
 function erf(x: real) : real;
@@ -11,20 +12,20 @@ const sqrtpi = 1.7724538;
       tol = 1.0E-4;
 
 var	x2,sum,sum1,term : real;
-	i : integer;
+	  i : integer;
 
 begin
-    x2 := x*x;
-    sum := x;
-    term := x;
-    i := 0;
-    repeat
-    i := i+1;
+  x2 := x * x;
+  sum := x;
+  term := x;
+  i := 0;
+  repeat
+    i := i + 1;
     sum1 := sum;
-    term := 2.0*term*x2/(1.0+2.0*i);
-    sum := term+sum1
-    until term<tol*sum;
-    erf := 2.0 * sum * exp(-x2) / sqrtpi
+    term := 2.0 * term * x2 / (1.0 + 2.0 * i);
+    sum := term + sum1
+  until term < tol * sum;
+  erf := 2.0 * sum * exp(-x2) / sqrtpi
 end; { erf }
 
 function erfc(x: real): real;
@@ -32,49 +33,49 @@ function erfc(x: real): real;
 const sqrtpi = 1.7724538;
       terms = 12;
 
-var	x2,u,v,sum : real;
+var	x2, u, v, sum : real;
     i : integer;
 begin
-    x2 := x*x;
-    v := 1.0 / (2.0 * x2);
-    u := 1.0 + v * (terms + 1.0);
-    for i := terms downto 1 do
-        begin
-            sum := 1.0 + i * v / u;
-            u := sum
-        end;
-    erfc := exp(-x2) / (x * sum * sqrtpi)
+  x2 := x * x;
+  v := 1.0 / (2.0 * x2);
+  u := 1.0 + v * (terms + 1.0);
+  for i := terms downto 1 do
+    begin
+      sum := 1.0 + i * v / u;
+      u := sum
+    end;
+  erfc := exp(-x2) / (x * sum * sqrtpi)
 end; { ercf }
 
 begin { main }
-    ClrScr;
-    done := false;
-    writeln;
-    repeat
-        write('Arg? ');
-        readln(x);
-        if x < 0.0 then done := true
-        else
-            begin
+  ClrScr;
+  done := false;
+  writeln;
+  repeat
+    write('Arg? ');
+    readln(x);
+      if x < 0.0 then done := true
+      else
+        begin
 	        if x = 0.0 then
-	            begin
-	                er := 0.0;
-	                ec := 1.0
-	            end
+	          begin
+	            er := 0.0;
+	            ec := 1.0
+	          end
 	        else
-	            begin
-	                if x < 1.5 then
-	                    begin
-		                    er := erf(x);
-		                    ec := 1.0 - er
-	                    end
-	                else
-	                    begin
-		                    ec := erfc(x);
-		                    er := 1.0 - ec
-	                end	{ if }
-	            end;
-	            writeln('X= ', x:8:4, ' Erf= ', er:12:8, ', Erfc= ', ec:12)
-            end	{ if }
-    until done
+	          begin
+	            if x < 1.5 then
+	              begin
+		              er := erf(x);
+		              ec := 1.0 - er
+	              end
+	            else
+	              begin
+		              ec := erfc(x);
+		              er := 1.0 - ec
+	              end	{ if }
+	          end;
+	        writeln('X= ', x:8:4, ' Erf= ', er:12:8, ', Erfc= ', ec:12)
+        end	{ if }
+  until done
 end.
