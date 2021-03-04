6d47258c5fb23cb669e8a509f9ff0be6894e898f
Mikhail Efremov <jakutenshi@gmail.com>
2021-03-04 12:30:26 +0300

    Add students to the scripts

---
+++ fetch.sh
@@ -0,0 +1,10 @@
+git fetch eduard1abdulmanov123
+git fetch esiole              
+git fetch AngelinaKalyaeva    
+git fetch godor1333           
+git fetch Evgenijjjj          
+git fetch levi218             
+git fetch Duyen38             
+git fetch NikitaPetaichuk     
+git fetch Villain123          
+git fetch smokfyz             
---
+++ remotes.sh
@@ -0,0 +1,10 @@
+git remote add eduard1abdulmanov123 https://github.com/eduard1abdulmanov123/spbetu_metrology_2021_7304.git
+git remote add esiole               https://github.com/esiole/spbetu_metrology_2021_7304.git
+git remote add AngelinaKalyaeva     https://github.com/AngelinaKalyaeva/spbetu_metrology_2021_7304.git
+git remote add godor1333            https://github.com/godor1333/spbetu_metrology_2021_7304.git
+git remote add Evgenijjjj           https://github.com/Evgenijjjj/spbetu_metrology_2021_7304.git
+git remote add levi218              https://github.com/levi218/spbetu_metrology_2021_7304.git
+git remote add Duyen38              https://github.com/Duyen38/spbetu_metrology_2021_7304.git
+git remote add NikitaPetaichuk      https://github.com/NikitaPetaichuk/spbetu_metrology_2021_7304.git
+git remote add Villain123           https://github.com/Villain123/spbetu_metrology_2021_7304.git
+git remote add smokfyz              https://github.com/smokfyz/spbetu_metrology_2021_7304.git
