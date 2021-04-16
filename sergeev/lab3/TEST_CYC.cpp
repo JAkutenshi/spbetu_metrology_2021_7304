b7eff4f4cb6734ae48e87aa69e549740101a0370
Mikhail Efremov <jakutenshi@gmail.com>
2021-04-16 03:45:17 +0300

    Sergeev lab3 is accepted

---
+++ sergeev/lab3/TEST_CYC.cpp
@@ -0,0 +1,54 @@
+																																																																																		#include <stdlib.h>
+#include "Sampler.h"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            							
+#define Size 10000            																																																																					
+
+int i, tmp, dim[Size];
+
+void main()
+{
+	SAMPLE;
+	for(i=0;i<Size/10;i++){ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/5;i++){ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/2;i++){ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size;i++)  { tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/10;i++)
+	{ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/5;i++)
+	{ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/2;i++)
+	{ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size;i++)
+	{ tmp=dim[0]; dim[0]=dim[i]; dim[i]=tmp; };
+	SAMPLE;
+	for(i=0;i<Size/10;i++)
+	{ tmp=dim[0];
+	 dim[0]=dim[i];
+	 dim[i]=tmp;
+	};
+	SAMPLE;
+	for(i=0;i<Size/5;i++)
+	{ tmp=dim[0];
+	 dim[0]=dim[i];
+	 dim[i]=tmp;
+	};
+	SAMPLE;
+	for(i=0;i<Size/2;i++)
+	{ tmp=dim[0];
+	 dim[0]=dim[i];
+	 dim[i]=tmp;
+	};
+	SAMPLE;
+	for(i=0;i<Size;i++)
+	{ tmp=dim[0];
+	 dim[0]=dim[i];
+	 dim[i]=tmp;
+	};
+	SAMPLE;
+}
